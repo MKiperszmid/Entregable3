ca21fe8e0031b3ed7a28d291569c96841cdc63fb
Martin Kiperszmid <kiper30@gmail.com>
2018-07-03 11:18:15 -0300

    Firebase Login + Crear Cuenta
    Se puede crear cuenta, o iniciar sesion en una existente.

--- app/src/main/java/com/example/com/entregable/View/Activities/MainActivity.java
+++ app/src/main/java/com/example/com/entregable/View/Activities/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.com.entregable.View.Activities;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.example.com.entregable.R;
@@ -12,6 +14,13 @@ import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 
@@ -22,12 +31,13 @@ public class MainActivity extends AppCompatActivity {
     private LoginButton loginButton;
 
     private static final String EMAIL = "email";
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mAuth = FirebaseAuth.getInstance();
         AccessToken accessToken = AccessToken.getCurrentAccessToken();
         isLoggedIn = accessToken != null && !accessToken.isExpired();
 
@@ -43,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                iniciarExhibicion();
+                handleFacebookAccessToken(loginResult.getAccessToken());
             }
 
             @Override
@@ -61,6 +71,89 @@ public class MainActivity extends AppCompatActivity {
     private void iniciarExhibicion(){
         Intent intent = new Intent(this, ExhibicionActivity.class);
         startActivity(intent);
+    }
+
+    private void createAccount(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("CREATEACCOUNT", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                            iniciarExhibicion();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("CREATEACCOUNT", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                        // ...
+                    }
+                });
+    }
+
+    private void singInAccount(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("SIGNIN", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                            iniciarExhibicion();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("SIGNIN", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                        // ...
+                    }
+                });
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d("HANDLEFB", "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("HANDLEFB", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            iniciarExhibicion();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("HANDLEFB", "signInWithCredential:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            //updateUI(currentUser);
+            iniciarExhibicion();
+        }
     }
 
     @Override
